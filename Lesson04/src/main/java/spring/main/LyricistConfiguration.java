0aa03a786117a1c08908938c576f2a1b0379fe21
KevinSmith <KevinSmish@users.noreply.github.com>
2018-08-21 09:49:40 +0300

    My first commit

---
+++ Lesson04/src/main/java/spring/main/LyricistConfiguration.java
@@ -0,0 +1,10 @@
+package spring.main;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan(basePackages = { "spring.impls" })
+@Configuration
+public class LyricistConfiguration {
+
+}
